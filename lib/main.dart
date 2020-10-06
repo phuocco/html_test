b1d08aab4c4aaf31976253a9e7808bb6adf63700
Phuoc Nguyen <vodanh.2901@gmail.com>
2020-10-06 14:04:44 +0700

    html hoạt động tốt nhưng video k ổn

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/html_parser.dart';
+import 'package:flutter_html/style.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: new ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'flutter_html Example'),
     );
   }
 }
@@ -30,84 +22,168 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
+const htmlData = """
+<h1>Header 1</h1>
+<h2>Header 2</h2>
+<h3>Header 3</h3>
+<h4>Header 4</h4>
+<h5>Header 5</h5>
+<h6>Header 6</h6>
+<h3>Ruby Support:</h3>
+      <p>
+        <ruby>
+          漢<rt>かん</rt>
+          字<rt>じ</rt>
+        </ruby>
+        &nbsp;is Japanese Kanji.
+      </p>
+      <h3>Support for <code>sub</code>/<code>sup</code></h3>
+      Solve for <var>x<sub>n</sub></var>: log<sub>2</sub>(<var>x</var><sup>2</sup>+<var>n</var>) = 9<sup>3</sup>
+      <p>One of the most <span>common</span> equations in all of physics is <br /><var>E</var>=<var>m</var><var>c</var><sup>2</sup>.</p>
+      <h3>Table support (with custom styling!):</h3>
+      <p>
+      <q>Famous quote...</q>
+      </p>
+      <table>
+      <colgroup>
+        <col width="50%" />
+        <col width="25%" />
+        <col width="25%" />
+      </colgroup>
+      <thead>
+      <tr><th>One</th><th>Two</th><th>Three</th></tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Data</td><td>Data</td><td>Data</td>
+      </tr>
+      <tr>
+        <td>Data</td><td>Data</td><td>Data</td>
+      </tr>
+      </tbody>
+      <tfoot>
+      <tr><td>fData</td><td>fData</td><td>fData</td></tr>
+      </tfoot>
+      </table>
+      <h3>Custom Element Support:</h3>
+      <flutter></flutter>
+      <flutter horizontal></flutter>
+      <h3>SVG support:</h3>
+      <svg id='svg1' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+            <circle r="32" cx="35" cy="65" fill="#F00" opacity="0.5"/>
+            <circle r="32" cx="65" cy="65" fill="#0F0" opacity="0.5"/>
+            <circle r="32" cx="50" cy="35" fill="#00F" opacity="0.5"/>
+      </svg>
+      <h3>List support:</h3>
+      <ol>
+            <li>This</li>
+            <li><p>is</p></li>
+            <li>an</li>
+            <li>
+            ordered
+            <ul>
+            <li>With<br /><br />...</li>
+            <li>a</li>
+            <li>nested</li>
+            <li>unordered
+            <ol>
+            <li>With a nested</li>
+            <li>ordered list.</li>
+            </ol>
+            </li>
+            <li>list</li>
+            </ul>
+            </li>
+            <li>list! Lorem ipsum dolor sit amet.</li>
+            <li><h2>Header 2</h2></li>
+            <h2><li>Header 2</li></h2>
+      </ol>
+      <h3>Link support:</h3>
+      <p>
+        Linking to <a href='https://github.com'>websites</a> has never been easier.
+      </p>
+      <h3>Image support:</h3>
+      <p>
+      
+      </p>
+      
+      <h3>Video support:</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/oeT4Z-C4Ux0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <video controls>
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" />
+      </video>
+      <h3>Audio support:</h3>
+      <audio controls>
+        <source src="https://www.w3schools.com/html/horse.mp3" />
+      </audio>
+
+     
+      
+""";
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return new Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('flutter_html Example'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+        child: Html(
+          data: htmlData,
+          //Optional parameters:
+          style: {
+            "html": Style(
+              backgroundColor: Colors.black12,
+//              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+//            "h1": Style(
+//              textAlign: TextAlign.center,
+//            ),
+            "table": Style(
+              backgroundColor: Color.fromARGB(0x50, 0xee, 0xee, 0xee),
             ),
-          ],
+            "tr": Style(
+              border: Border(bottom: BorderSide(color: Colors.grey)),
+            ),
+            "th": Style(
+              padding: EdgeInsets.all(6),
+              backgroundColor: Colors.grey,
+            ),
+            "td": Style(
+              padding: EdgeInsets.all(6),
+            ),
+            "var": Style(fontFamily: 'serif'),
+          },
+          customRender: {
+            "flutter": (RenderContext context, Widget child, attributes, _) {
+              return FlutterLogo(
+                style: (attributes['horizontal'] != null)
+                    ? FlutterLogoStyle.horizontal
+                    : FlutterLogoStyle.markOnly,
+                textColor: context.style.color,
+                size: context.style.fontSize.size * 5,
+              );
+            },
+          },
+          onLinkTap: (url) {
+            print("Opening $url...");
+          },
+          onImageTap: (src) {
+            print(src);
+          },
+          onImageError: (exception, stackTrace) {
+            print(exception);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
