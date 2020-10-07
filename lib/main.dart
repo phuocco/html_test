fe6637646e38f8be4430d3325440360806a00952
Phuoc Nguyen <vodanh.2901@gmail.com>
2020-10-07 17:13:57 +0700

    ngon

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 void main() {
   runApp(MyApp());
@@ -50,57 +52,58 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: HtmlWidget(
+          // the first parameter (`html`) is required
+          '''
+  <h3>Heading</h3>
+  <p class="foo">
+    A paragraph with <strong>strong</strong>, <em>emphasized</em>
+    and <span style="color: red">colored</span> text.
+  </p>
+  <source id="youtube" src="https://www.youtube.com/embed/0CtV6x5N8ic"></source>
+  <p id="youtube"> a </p>
+  ''',
+
+          customStylesBuilder: (element) {
+            if (element.classes.contains('foo')) {
+              return {'color': 'red'};
+            }
+            return null;
+          },
+
+          customWidgetBuilder: (element) {
+            if (element.id == 'youtube') {
+              print("aaa");
+              String a = element.outerHtml;
+              print(a);
+              YoutubePlayerController _controller = YoutubePlayerController(
+                initialVideoId: '0CtV6x5N8ic',
+                params: YoutubePlayerParams(
+                  showControls: true,
+                  showFullscreenButton: true,
+                ),
+              );
+              return YoutubePlayerIFrame(
+                controller: _controller,
+              );
+            }
+            return null;
+          },
+
+          textStyle: TextStyle(fontSize: 14),
+
+          webView: true,
         ),
       ),
       floatingActionButton: FloatingActionButton(
